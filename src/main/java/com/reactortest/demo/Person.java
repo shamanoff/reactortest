f8846ca3a7657a0e64dedaa30d650cf1742c74e1
Shamanoff <shamanoff@gmail.com>
2017-10-26 15:40:58 +0300

    Initial commit

---
+++ src/main/java/com/reactortest/demo/Person.java
@@ -0,0 +1,18 @@
+package com.reactortest.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+
+    private String firstName;
+    private String lastName;
+
+    public String sayMyName() {
+        return "My Name is " + firstName + " " + lastName + ".";
+    }
+}
